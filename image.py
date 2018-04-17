c188b4acc9f7b4cccebb219e228b8adc9569a523
cesare <cesare.cugnasco@gmail.com>
2018-04-17 12:55:55 +0200

    initial work on hecuba integration

--- image.py
+++ image.py
@@ -9,14 +9,14 @@ import scipy.ndimage
 from my_utils import get_logger, dtype_limits, isstring
 
 from bounding_box import BoundingBox
+
 try:
     import cv2
+
     CV2_AVAILABLE = True
 except ImportError:
     print("image: Module cv2 is not available, using scikit-image as a fallback")
     CV2_AVAILABLE = False
-
-
 
 # modes for opening an Image
 READONLY = 'r'
@@ -26,6 +26,7 @@ READWRITE = 'r+'
 NEAREST = 0
 LINEAR = 1
 CUBIC = 3
+
 
 def resize(img, shape, interpolation=CUBIC, antialias=True):
     """Resize the image to specified shape using the given interpolation.
@@ -49,7 +50,7 @@ def resize(img, shape, interpolation=CUBIC, antialias=True):
     if antialias and interpolation != NEAREST:
         # if target shape is larger than image shape, sigma will be 0 (no filtering)
         filter_width = np.round(np.asarray(img.shape, dtype=float) / np.asarray(shape) * 2).astype(int)
-        sigma = (filter_width-1)/2. - 0.5
+        sigma = (filter_width - 1) / 2. - 0.5
         arr = scipy.ndimage.filters.gaussian_filter(img, sigma=sigma, truncate=1)
 
     if CV2_AVAILABLE:
@@ -59,8 +60,6 @@ def resize(img, shape, interpolation=CUBIC, antialias=True):
         res = skimage.transform.resize(arr, shape[:2], order=interpolation, preserve_range=True, mode="constant")
 
     return res.astype(img.dtype)
-
-
 
 
 def open_image(filename, page=0):
@@ -94,9 +93,9 @@ def open_image(filename, page=0):
             # After transposing, we have to adjust the dimensions
             img = img.reshape(shape[2], shape[3])
     else:
-        img = skimage.io.imread(filename)  #, as_grey=True)
+        img = skimage.io.imread(filename)  # , as_grey=True)
     # if rgb, convert to grayscale
-    #if len(img.shape) > 2:
+    # if len(img.shape) > 2:
     #    img = skimage.color.rgb2gray(img)
     return img
 
@@ -118,11 +117,11 @@ def get_maxintensity(filename, attributePath=None):
     else:
         img = filename
     if isinstance(img, pytiff._pytiff.Tiff):
-        maxintensity = 2.**img.n_bits
+        maxintensity = 2. ** img.n_bits
     elif isinstance(img, h5py._hl.dataset.Dataset):
         maxintensity = img[attributePath].attrs["maxintensity"]
     else:
-        maxvalues = np.array([2**b-1 for b in [8,16,32,64]])
+        maxvalues = np.array([2 ** b - 1 for b in [8, 16, 32, 64]])
         imgmax = np.amax(img)
         maxintensity = maxvalues[np.where(maxvalues - imgmax >= 0)[0][0]]
         if imgmax < 1:
@@ -132,13 +131,13 @@ def get_maxintensity(filename, attributePath=None):
     return maxintensity
 
 
-
 class Image(object):
     """Basic Image class. Implemented by Hdf5Image, TiffImage and MemoryImage.
 
     It has some basic functionality on it's own, but it does not make
     a lot of sense to use this class directly.
     """
+
     def __init__(self, spacing, size, scales, maxintensity=255):
         """
         Args:
@@ -158,7 +157,7 @@ class Image(object):
             # avoid overflow when calcuating maxintensity
             self._maxintensity = maxintensity
         else:
-            self._maxintensity = 2**math.ceil(math.log(maxintensity+1,2))-1
+            self._maxintensity = 2 ** math.ceil(math.log(maxintensity + 1, 2)) - 1
 
     def __repr__(self):
         return 'Image(spacing={}, size={}, scales={}, maxintensity={})'.format(
@@ -180,10 +179,12 @@ class Image(object):
                 return Hdf5Image(filename, prefix=prefix, spacing=spacing, maxintensity=maxintensity)
             elif filename.endswith('.tif') or filename.endswith('.tiff'):
                 return TiffImage(filename, spacing=spacing, maxintensity=maxintensity)
+            elif filename.endswith('.hecuba'):
+                return HecubaImage(filename, spacing=spacing, maxintensity=maxintensity)
             else:
                 img = open_image(filename)
                 return MemoryImage(img, spacing=spacing, maxintensity=maxintensity)
- 
+
     @property
     def scales(self):
         """Scales that are available for this image."""
@@ -205,7 +206,7 @@ class Image(object):
         Args:
             spacing (int, optional): spacing used to calculate the size (in um).
         """
-        size = tuple(map(lambda s: int(s*self._spacing/float(spacing)), self._size))
+        size = tuple(map(lambda s: int(s * self._spacing / float(spacing)), self._size))
         return size
 
     def _get_available_scale(self, spacing):
@@ -226,8 +227,7 @@ class Image(object):
     def scale_to_spacing(self, scale):
         return self._spacing / float(scale)
 
-
-    #TODO problems if blocksize * scale is not a natural number.
+    # TODO problems if blocksize * scale is not a natural number.
     @staticmethod
     def crop_from_array(arr, bbox, arr_spacing=1., wanted_spacing=1., interpolation=CUBIC, dtype=None, blocksize=8000):
         """Crop bbox from arr and rescale to wanted_spacing.
@@ -253,7 +253,8 @@ class Image(object):
             bbox = BoundingBox.copy(bbox)
             bbox.set_to_spacing(arr_spacing)
         assert bbox.spacing == arr_spacing
-        output_size = tuple([int(math.floor(s * arr_spacing/wanted_spacing)) for s in bbox.get_hw(round_values=False)])
+        output_size = tuple(
+            [int(math.floor(s * arr_spacing / wanted_spacing)) for s in bbox.get_hw(round_values=False)])
         if len(arr.shape) > 2:
             # Allow cropping from arrays with more than 2 dimensions
             output_size = output_size + arr.shape[2:]
@@ -261,30 +262,33 @@ class Image(object):
             result_crop = np.zeros(output_size, dtype=arr.dtype)
         else:
             result_crop = np.zeros(output_size, dtype=dtype)
-        if not (blocksize * arr_spacing/wanted_spacing).is_integer():
-            log.warning("blocksize * scale is not a natural number:{}. This may lead to problems in blockwise resizing.".format(blocksize * arr_spacing/wanted_spacing))
-        for box in blocks(bbox,(blocksize,blocksize)):
+        if not (blocksize * arr_spacing / wanted_spacing).is_integer():
+            log.warning(
+                "blocksize * scale is not a natural number:{}. This may lead to problems in blockwise resizing.".format(
+                    blocksize * arr_spacing / wanted_spacing))
+        for box in blocks(bbox, (blocksize, blocksize)):
             # box is part of bbox, has to be moved by vector of upper left corner
-            box.set_center(np.array(box.center)+np.array(bbox.get_corners(round_values=False)[0]))
-            output_size = tuple([int(math.floor(s * (arr_spacing/wanted_spacing))) for s in box.get_hw(round_values=False)])
+            box.set_center(np.array(box.center) + np.array(bbox.get_corners(round_values=False)[0]))
+            output_size = tuple(
+                [int(math.floor(s * (arr_spacing / wanted_spacing))) for s in box.get_hw(round_values=False)])
             if min(output_size) == 0: continue
-            #pyextrae.eventandcounters(5000, 20)
+            # pyextrae.eventandcounters(5000, 20)
             crop = np.array(box.crop_from_array(arr))
-            #pyextrae.eventandcounters(5000, 0)
+            # pyextrae.eventandcounters(5000, 0)
             if arr_spacing != wanted_spacing:
                 # rescale the crop to fit to output_size
-                #log.debug("crop_from_array is reshaping from {} to {}".format(crop.shape, output_size))
+                # log.debug("crop_from_array is reshaping from {} to {}".format(crop.shape, output_size))
                 crop = resize(crop, output_size, interpolation=interpolation, antialias=True)
-                assert crop.shape[:2] == output_size, "{} {}".format(crop.shape,output_size)
+                assert crop.shape[:2] == output_size, "{} {}".format(crop.shape, output_size)
 
             if dtype is not None:
                 crop = crop.astype(np.float64) / get_maxintensity(arr) * dtype_limits(dtype)[1]
                 crop = crop.astype(dtype)
             # move box back to origin and insert crop into result_crop
-            box.set_center(np.array(box.center)-np.array(bbox.get_corners(round_values=False)[0]))
-            box.rescale(arr_spacing/wanted_spacing)
-            box.set_hw(box.get_hw()[0],box.get_hw()[1])
-            box.write_to_array(result_crop,crop)
+            box.set_center(np.array(box.center) - np.array(bbox.get_corners(round_values=False)[0]))
+            box.rescale(arr_spacing / wanted_spacing)
+            box.set_hw(box.get_hw()[0], box.get_hw()[1])
+            box.write_to_array(result_crop, crop)
         return result_crop
 
 
@@ -355,10 +359,10 @@ class Hdf5Image(Image):
 
         # TODO temporary workarounds for HANNAH
         elif 'scale0' in dset.attrs:
-            spacing = 1./dset.attrs['scale0']
+            spacing = 1. / dset.attrs['scale0']
             log.debug('found depreceated scale0 in file {} (setting spacing to {})'.format(filename, spacing))
         elif 'scale' in dset.attrs:
-            spacing = 1./dset.attrs['scale']
+            spacing = 1. / dset.attrs['scale']
             log.debug('found depreceated scale in file {} (setting spacing to {})'.format(filename, spacing))
 
         if spacing is None:
@@ -471,9 +475,9 @@ class TiffImage(Image):
             img.set_page(page)
             levels.append(img)
         scales = [shape[0] / float(shapes[0][0]) for shape in shapes]
-        #log.debug("TiffImage - Scales before rounding: %s", scales)
-        scales = [1/2**round(math.log(1/scale, 2)) for scale in scales]
-        #log.debug("TiffImage - Scales after rounding: %s", scales)
+        # log.debug("TiffImage - Scales before rounding: %s", scales)
+        scales = [1 / 2 ** round(math.log(1 / scale, 2)) for scale in scales]
+        # log.debug("TiffImage - Scales after rounding: %s", scales)
         if maxintensity is None:
             maxintensity = dtype_limits(f.dtype)[1]
         size = shapes[0][:2]
@@ -532,6 +536,7 @@ class TiffImage(Image):
             crop = crop.astype(dtype)
         return crop
 
+
 def blocks(data, size):
     """A generator yielding sliding bounding boxes with a given size over given data.
 
@@ -544,14 +549,13 @@ def blocks(data, size):
     """
     size = np.array(size)
     shape = np.array(data.shape)
-    n_rows, n_cols = np.ceil(shape[:2]/size).astype(int)
+    n_rows, n_cols = np.ceil(shape[:2] / size).astype(int)
     for r in range(n_rows):
         for c in range(n_cols):
             # swap row and column, x = column, y = row
-            upper_left = (c*size[1], r * size[0])
-            lower_right = (min((c+1)*size[1], shape[1]), min((r+1)*size[0], shape[0]))
+            upper_left = (c * size[1], r * size[0])
+            lower_right = (min((c + 1) * size[1], shape[1]), min((r + 1) * size[0], shape[0]))
             yield BoundingBox.from_corners(upper_left, lower_right)
-
 
 
 class MemoryImage(Image):
@@ -601,6 +605,63 @@ class MemoryImage(Image):
         crop = Image.crop_from_array(img, bbox, img_spacing, spacing, interpolation, dtype)
         return crop
 
+
+from hecuba import StorageDict
+
+
+class ImgLevels(StorageDict):
+    '''
+    @ClassType <<id:int>,value:numpy.ndarray>
+    '''
+    pass
+
+
+class HecubaImage(Image):
+    """Represents an image stored with Hecuba.
+
+    Supports creation of pyramids, extraction of crops and saving as tiff or hdf5 image.
+    """
+
+    def __init__(self, filename, spacing=1., maxintensity=None):
+        """ Initialize MemoryImage.
+
+        Args:
+            arr (array-like): the image.
+            spacing (float): spacing of the image.
+            maxintensity (int): maxintensity of pixel values in the image.
+        """
+        levels = ImgLevels(filename)
+        scales = [1.]
+        if maxintensity is None:
+            maxintensity = dtype_limits(levels[0].dtype)[1]
+
+        size = levels[0].shape[:2]
+        super(HecubaImage, self).__init__(spacing, size, scales, maxintensity)
+        self._levels = levels
+
+    def __repr__(self):
+        return 'HecubaImage(arr={}, spacing={}, maxintensity={})'.format(
+            self._levels[0], self.spacing, self.maxintensity)
+
+    def get_crop(self, bbox, spacing, dtype=None, interpolation=CUBIC):
+        """Crop bbox from image.
+
+        If a dtype is given, the returned values are divided by the maxintensity and scaled
+        to the dtype.
+
+        Args:
+            bbox (BoundingBox): box with spacing box.spacing. If none, the whole image is cropped.
+            spacing (number): spacing that the resulting crop should have.
+            dtype (numpy dtype): integer dtype of the resulting crop.
+            interpolation (string): interpolation method for eventual rescale of the crop.
+        Returns:
+            (np.ndarray): cropped image.
+        """
+        scale = self._get_available_scale(spacing)
+        img = self._levels[list(self.scales).index(scale)]
+        img_spacing = self.scale_to_spacing(scale)
+        crop = Image.crop_from_array(img, bbox, img_spacing, spacing, interpolation, dtype)
+        return crop
 
 
 def crop_from_image(bbox, f, scale=1., prefix='/', interpolation=CUBIC, dtype=None):
